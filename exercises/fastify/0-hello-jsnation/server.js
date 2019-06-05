824664466a2ddd45210f4917c061bc3043c2c767
David Mark Clements <huperekchuno@googlemail.com>
2019-06-05 23:05:48 +0200

    transition slides

--- exercises/fastify/0-hello-jsnation/server.js
+++ exercises/fastify/0-hello-jsnation/server.js
@@ -4,9 +4,7 @@ const app = require('fastify')({
   logger: true
 })
 
-app.get('/', async (request, reply) => {
-  return 'hello jsnation'
-})
+app.get('/', async () => 'hello jsnation')
 
 // Run the server!
 app.listen(3000).catch(console.error)
