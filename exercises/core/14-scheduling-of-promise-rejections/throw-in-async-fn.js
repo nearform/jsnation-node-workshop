9a5d86f6dc860c22b960567b4237c3ffa3636809
Matteo Collina <hello@matteocollina.com>
2019-06-06 10:57:56 +0200

    semicolons are great

--- exercises/core/14-scheduling-of-promise-rejections/throw-in-async-fn.js
+++ exercises/core/14-scheduling-of-promise-rejections/throw-in-async-fn.js
@@ -1,5 +1,6 @@
 'use strict'
-(async () => { throw Error('problems. ') })()
+
+;(async () => { throw Error('problems. ') })()
 setImmediate(() => console.log('1'))
 process.nextTick(() => console.log('2'))
 console.log('3')
